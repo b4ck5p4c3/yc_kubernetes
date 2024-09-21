9f43fc12a0b72336fa21737889ef26375236c0c6
Polina Katunina <katunina0polina@gmail.com>
2024-09-22 00:22:51 +0300

    add three instances

--- default.tf
+++ default.tf
@@ -7,24 +7,53 @@ terraform {
   required_version = ">= 0.13"
 }
 
-provider "yandex" {
-  zone = "ru-central1-a"
+variable "zones" {
+  type = list(string)
+  default = ["ru-central1-a", "ru-central1-b", "ru-central1-d"]
 }
 
-resource "yandex_compute_disk" "kuber-a-disk" {
-  name     = "kuber-a"
-  type     = "network-ssd-nonreplicated"
-  zone     = "ru-central1-a"
+resource "yandex_compute_disk" "kuber_disks" {
+  count = length(var.zones)
+
+  name     = "kuber-${count.index}"
+  type     = "network-ssd"
+  zone     = var.zones[count.index]
   size     = "93"
   image_id = "fd83h72fb5urnmt6vkfd"
 }
 
-resource "yandex_compute_placement_group" "kuber" {
+resource "yandex_compute_placement_group" "kuber_pg" {
     name = "kuber"
 }
 
-resource "yandex_compute_instance" "kuber-a" {
-  name = "terraform1"
+resource "yandex_vpc_network" "kuber_net" {
+  name = "kuber_network"
+}
+
+resource "yandex_vpc_subnet" "kuber_subnets" {
+  count = length(var.zones)
+  
+  v4_cidr_blocks = ["10.1${count.index}.0.0/24"]
+  zone           = var.zones[count.index]
+  network_id     = yandex_vpc_network.kuber_net.id
+}
+
+resource "yandex_vpc_address" "kuber_addresses" {
+  count = length(var.zones)
+  
+  name = "kuber_addr${count.index}"
+
+  external_ipv4_address {
+    zone_id = var.zones[count.index]
+  }
+}
+
+resource "yandex_compute_instance" "kuber_instances" {
+  count = length(var.zones)
+
+  name = "terraform${count.index}"
+  zone = var.zones[count.index]
+  platform_id = var.zones[count.index] == "ru-central1-d" ? "standard-v3" : "standard-v1"
   
   resources {
     cores  = 2
@@ -32,20 +61,24 @@ resource "yandex_compute_instance" "kuber-a" {
   }
 
   boot_disk {
-    disk_id = yandex_compute_disk.kuber-a-disk.id
+    disk_id = yandex_compute_disk.kuber_disks[count.index].id
   }
 
   network_interface {
-    subnet_id = "e9brf3q098gs97db97ma"
+    subnet_id = yandex_vpc_subnet.kuber_subnets[count.index].id
+    nat = true
+    nat_ip_address = yandex_vpc_address.kuber_addresses[count.index].external_ipv4_address[0].address
   }
 
   placement_policy {
-    placement_group_id = yandex_compute_placement_group.kuber.id
+    placement_group_id = yandex_compute_placement_group.kuber_pg.id
   }
 
   scheduling_policy {
     preemptible = true
   }
+
+  metadata = {
+    enable-oslogin = true
+  }
 }
-
-
