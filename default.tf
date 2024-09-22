aed9588b56a9b1d82f708a421d09b338f97eb4e2
Polina Katunina <katunina0polina@gmail.com>
2024-09-22 22:54:38 +0300

    add hardcoded ssh key (whoops)

--- default.tf
+++ default.tf
@@ -109,6 +109,7 @@ resource "yandex_compute_instance" "kuber_instances" {
 
   metadata = {
     enable-oslogin = true
+    ssh-keys = "ubuntu:ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICRggREl9QpxjSp/kZzFp6XS9dvhfntcH6sZs1dtN6Q7"
   }
 }
 
