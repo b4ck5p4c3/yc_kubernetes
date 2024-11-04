bb7b0b81bfc4ed748658f71da3e18dc46d631898
Polina Katunina <katunina0polina@gmail.com>
2024-11-04 18:28:42 +0300

    fix script

--- env_prepare.sh
+++ env_prepare.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-export AWS_ACCESS_KEY_ID=$(yc lockbox payload get --name terraform_s3_keys --format json | jq '.entries[] | select(.key == "AWS_ACCESS_KEY_ID") | .text_value')
-export AWS_SECRET_ACCESS_KEY=$(yc lockbox payload get --name terraform_s3_keys --format json | jq '.entries[] | select(.key == "AWS_SECRET_ACCESS_KEY") | .text_value')
+export AWS_ACCESS_KEY_ID=$(yc lockbox payload get --name terraform_s3_keys --format json | jq -r '.entries[] | select(.key == "AWS_ACCESS_KEY_ID") | .text_value')
+export AWS_SECRET_ACCESS_KEY=$(yc lockbox payload get --name terraform_s3_keys --format json | jq -r '.entries[] | select(.key == "AWS_SECRET_ACCESS_KEY") | .text_value')
 
 export YC_TOKEN=$(yc iam create-token)
 export YC_CLOUD_ID=$(yc config get cloud-id)
